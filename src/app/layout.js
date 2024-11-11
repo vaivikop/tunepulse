245b95b69f83030b45adb7f96f0c2cb92cbb4bb9
vaivikop <89904607+vaivikop@users.noreply.github.com>
2024-11-11 13:15:29 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,14 +17,14 @@ const poppins = Poppins({
 });
 
 export const metadata = {
-  title: "Hayasaka",
+  title: "TunePulse",
   description: "Music streaming app",
   image:
     "https://res.cloudinary.com/dbr73rpz9/image/upload/v1690380865/images/logo-color_noktgr.png",
-  url: "https://hayasaka.vercel.app",
+  url: "https://tunepulse.vercel.app",
   type: "website",
   icons: [{ rel: "icon", url: Favicon.src }],
-  site_name: "Hayasaka",
+  site_name: "TunePulse",
   manifest: "/manifest.json",
 };
 
