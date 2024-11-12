3d7312e9bb29ba287e695971df8d4c0dc9c8a9bb
vaivik <minecraftworld16764@gmail.com>
2024-11-12 15:20:11 +0530

    added my name

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -25,60 +25,69 @@ const Sidebar = ({ showNav, setShowNav }) => {
     document.body.removeChild(link);
   };
 
-  return (
-    <div
-      className={`${
-        showNav ? "" : "translate-x-[-100%]"
-      } transition-all duration-200  h-screen lg:w-[300px] md:w-[250px] w-[65vw] fixed top-0 left-0 z-40 bg-[#020813] flex flex-col justify-between`}
-    >
-      <div>
-        <div className=" flex mt-3">
-          <MdOutlineMenu
-            onClick={() => setShowNav(false)}
-            className=" mx-4 text-2xl lg:text-3xl my-auto text-white cursor-pointer"
-          />
-          <div className=" flex justify-center items-center">
-            <Link href="/">
-              <Image
-                onClick={() => {
-                  dispatch(setProgress(100));
-                }}
-                src={logoWhite}
-                alt="logo"
-                className=" lg:py-2  aspect-video w-[139px] h-[40px] lg:h-[62px] lg:w-[190px]"
-              />
-            </Link>
-          </div>
-        </div>
-        <div className=" mt-7 pb-7 border-b border-gray-400 w-[95%]">
-          <Profile setShowNav={setShowNav} />
-        </div>
-        <div className="flex flex-col gap-1">
-          <Languages />
-          <hr className="border-gray-400 w-[95%] mx-auto" />
-        </div>
-        <Favourites setShowNav={setShowNav} />
-        <div className="flex flex-col gap-1">
-          <hr className="border-gray-400 w-[95%] mx-auto" />
-          <Playlists setShowNav={setShowNav} />
-          <hr className="border-gray-400 w-[95%] mx-auto" />
+  // Other imports and code above remain the same
+
+return (
+  <div
+    className={`${
+      showNav ? "" : "translate-x-[-100%]"
+    } transition-all duration-200 h-screen lg:w-[300px] md:w-[250px] w-[65vw] fixed top-0 left-0 z-40 bg-[#020813] flex flex-col justify-between`}
+  >
+    <div>
+      <div className="flex mt-3">
+        <MdOutlineMenu
+          onClick={() => setShowNav(false)}
+          className="mx-4 text-2xl lg:text-3xl my-auto text-white cursor-pointer"
+        />
+        <div className="flex justify-center items-center">
+          <Link href="/">
+            <Image
+              onClick={() => {
+                dispatch(setProgress(100));
+              }}
+              src={logoWhite}
+              alt="logo"
+              className="lg:py-2 aspect-video w-[139px] h-[40px] lg:h-[62px] lg:w-[190px]"
+            />
+          </Link>
         </div>
       </div>
-      <div className=" mb-28 text-gray-200 mx-3 flex gap-3">
-        <Link href="/dmca">
-          <p className="hover:border border-gray-200 p-1 font-medium w-fit rounded cursor-pointer text-sm">
-            DMCA
-          </p>
-        </Link>
-        <button
-          onClick={handleDownload}
-          className="hover:border border-gray-200 p-1 font-medium w-fit rounded cursor-pointer text-sm"
-        >
-          Download App
-        </button>
+      <div className="mt-7 pb-7 border-b border-gray-400 w-[95%]">
+        <Profile setShowNav={setShowNav} />
+      </div>
+      <div className="flex flex-col gap-1">
+        <Languages />
+        <hr className="border-gray-400 w-[95%] mx-auto" />
+      </div>
+      <Favourites setShowNav={setShowNav} />
+      <div className="flex flex-col gap-1">
+        <hr className="border-gray-400 w-[95%] mx-auto" />
+        <Playlists setShowNav={setShowNav} />
+        <hr className="border-gray-400 w-[95%] mx-auto" />
       </div>
     </div>
-  );
+    
+    {/* New "Created with ❤️ by Vaivik Shah" line */}
+    <div className="text-center text-gray-200 mb-2">
+      <p className="text-sm">Created with ❤️ by Vaivik Shah</p>
+    </div>
+
+    <div className="mb-28 text-gray-200 mx-3 flex gap-3">
+      <Link href="/dmca">
+        <p className="hover:border border-gray-200 p-1 font-medium w-fit rounded cursor-pointer text-sm">
+          DMCA
+        </p>
+      </Link>
+      <button
+        onClick={handleDownload}
+        className="hover:border border-gray-200 p-1 font-medium w-fit rounded cursor-pointer text-sm"
+      >
+        Download App
+      </button>
+    </div>
+  </div>
+);
+
 };
 
 export default Sidebar;
