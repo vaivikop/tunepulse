46a0d6548a308aa1e75d938c424d09737a29927b
vaivik <minecraftworld16764@gmail.com>
2024-11-18 11:14:16 +0530

    added settings

---
+++ src/app/settings/page.jsx
@@ -0,0 +1,15 @@
+'use client';
+import React from 'react';
+
+const Settings = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-semibold text-white">Settings</h1>
+      <p className="text-gray-400 mt-2">Manage your account and preferences.</p>
+
+      {/* Add settings sections here */}
+    </div>
+  );
+};
+
+export default Settings;
