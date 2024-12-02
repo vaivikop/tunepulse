33a5e9ce6deb08e8468a849ac75b75cb31968459
vaivik <minecraftworld16764@gmail.com>
2024-12-02 20:43:48 +0530

    updated email

--- src/utils/mailSender.js
+++ src/utils/mailSender.js
@@ -5,7 +5,8 @@ const mailSender = async (email, title, body) => {
     try{
             let transporter = nodemailer.createTransport({
                 host:process.env.MAIL_HOST,
-                port: 587,
+                port: 465,
+                secure: true,
                 auth:{
                     user: process.env.MAIL_USER,
                     pass: process.env.MAIL_PASS,
