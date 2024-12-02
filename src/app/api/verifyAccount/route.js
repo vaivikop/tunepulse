7218ae6f3b9f9f63588008c56a306c84761cad9e
vaivik <minecraftworld16764@gmail.com>
2024-12-02 20:14:14 +0530

    update

--- src/app/api/verifyAccount/route.js
+++ src/app/api/verifyAccount/route.js
@@ -73,6 +73,8 @@ export async function POST(req) {
                 <p>Hello ${user.userName},</p>
                 <p>Thank you for joining TunePulse! Please verify your account by clicking the button below:</p>
                 <a href="${verificationUrl}" class="button">Verify Account</a>
+                <p>If the button above doesn't work, you can copy and paste the following link into your browser:</p>
+                <p><a href="${verificationUrl}" style="color: #1da1f2; text-decoration: none;">${verificationUrl}</a></p>
                 <p>If you didn't create this account, please ignore this email.</p>
                 <p>This verification link will expire in 10 minutes.</p>
                 <div class="footer">
